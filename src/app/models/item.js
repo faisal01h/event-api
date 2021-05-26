98a0bfc54fa48d37da90c8eb18c35ceced23aa36
Faisal Hanif <faisal01h@gmail.com>
2021-05-26 22:58:00 +0700

    restructured models

--- src/app/models/item.js
+++ src/app/models/item.js
@@ -3,10 +3,6 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const Item = new Schema({
-    id: {
-        type: Number,
-        required: true
-    },
     title: {
         type: String,
         required: true
